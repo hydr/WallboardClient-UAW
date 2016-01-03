65675f663e80a3b6e20681249b5f508745cb607a
Maximilian Balbach <m.balbach@crossvertise.com>
2016-01-03 23:11:59 +0100

    Initial Commit

---
+++ WallboardClient/MainPage.xaml.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// Die Vorlage "Leere Seite" ist unter http://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409 dokumentiert.
+
+namespace WallboardClient
+{
+    /// <summary>
+    /// Eine leere Seite, die eigenständig verwendet oder zu der innerhalb eines Rahmens navigiert werden kann.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        public MainPage()
+        {
+            this.InitializeComponent();
+            MyWebView.Navigate(new Uri("http://www.crossvertise.com"));
+        }
+
+        private void LoadButton_Click(object sender, RoutedEventArgs e)
+        {
+            MyWebView.Navigate(new Uri(Address.Text));
+        }
+
+        private void RefreshButton_Click(object sender, RoutedEventArgs e)
+        {
+            MyWebView.Refresh();
+        }
+    }
+}
